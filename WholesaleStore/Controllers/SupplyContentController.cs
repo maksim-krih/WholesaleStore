a22fab3ee96be573d864d0dafb3d4f3b756da0c6
Maksym Krykhovetskyi <maksym.krykhovetskyi@inoxoft.com>
2020-05-18 22:22:32 +0300

    added supply, supply content, supply shipment

---
+++ WholesaleStore/Controllers/SupplyContentController.cs
@@ -0,0 +1,175 @@
+﻿using System.Data.Entity;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using WholesaleStore.Controllers.Base;
+using WholesaleStore.Data.Interfaces;
+
+namespace WholesaleStore.Controllers
+{
+    public class SupplyContentController : BaseController
+    {
+        public SupplyContentController(
+            IDataExecutor dataExecutor,
+            IGridManager gridManager,
+            IDataBaseManager dataBaseManager) : base(gridManager, dataExecutor, dataBaseManager)
+        {
+
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var supplyContents = await _dataExecutor.ToListAsync(_dataBaseManager.SupplyContentRepository.Query.Include(s => s.Product).Include(s => s.Supply));
+
+            return View(supplyContents);
+        }
+
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            
+            var supplyContent = await _dataExecutor.FirstOrDefaultAsync(
+                _dataBaseManager.SupplyContentRepository.Query
+                .Include(s => s.Product)
+                .Include(s => s.Supply),
+                x => x.Id == id);
+
+            if (supplyContent == null)
+            {
+                return HttpNotFound();
+            }
+            
+            return View(supplyContent);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.ProductId = new SelectList(_dataBaseManager.ProductRepository.Query, "Id", "Name");
+            ViewBag.SupplyId = new SelectList(_dataBaseManager.SupplyRepository.Query, "Id", "Id");
+            
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(SupplyContent supplyContent)
+        {
+            if (ModelState.IsValid)
+            {
+                _dataBaseManager.SupplyContentRepository.Create(supplyContent);
+
+                await _dataBaseManager.SupplyContentRepository.CommitAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProductId = new SelectList(_dataBaseManager.ProductRepository.Query, "Id", "Name", supplyContent.ProductId);
+            ViewBag.SupplyId = new SelectList(_dataBaseManager.SupplyRepository.Query, "Id", "Id", supplyContent.SupplyId);
+            
+            return View(supplyContent);
+        }
+
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var supplyContent = await _dataExecutor.FirstOrDefaultAsync(
+                _dataBaseManager.SupplyContentRepository.Query
+                .Include(s => s.Product)
+                .Include(s => s.Supply),
+                x => x.Id == id);
+
+            if (supplyContent == null)
+            {
+                return HttpNotFound();
+            }
+            
+            ViewBag.ProductId = new SelectList(_dataBaseManager.ProductRepository.Query, "Id", "Name", supplyContent.ProductId);
+            ViewBag.SupplyId = new SelectList(_dataBaseManager.SupplyRepository.Query, "Id", "Id", supplyContent.SupplyId);
+            
+            return View(supplyContent);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(SupplyContent supplyContent)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = await _dataExecutor.FirstOrDefaultAsync(
+                _dataBaseManager.SupplyContentRepository.Query
+                .Include(s => s.Product)
+                .Include(s => s.Supply),
+                x => x.Id == supplyContent.Id);
+
+                entity.SupplyId = supplyContent.SupplyId;
+                entity.ProductId = supplyContent.ProductId;
+                entity.Count = supplyContent.Count;
+                entity.SupplyPrice = supplyContent.SupplyPrice;
+
+                await _dataBaseManager.SupplyContentRepository.CommitAsync();
+
+                return RedirectToAction("Index");
+            }
+            
+            ViewBag.ProductId = new SelectList(_dataBaseManager.ProductRepository.Query, "Id", "Name", supplyContent.ProductId);
+            ViewBag.SupplyId = new SelectList(_dataBaseManager.SupplyRepository.Query, "Id", "Id", supplyContent.SupplyId);
+            
+            return View(supplyContent);
+        }
+
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var supplyContent = await _dataExecutor.FirstOrDefaultAsync(
+                _dataBaseManager.SupplyContentRepository.Query
+                .Include(s => s.Product)
+                .Include(s => s.Supply),
+                x => x.Id == id);
+
+            if (supplyContent == null)
+            {
+                return HttpNotFound();
+            }
+         
+            return View(supplyContent);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            var supplyContent = await _dataExecutor.FirstOrDefaultAsync(
+                _dataBaseManager.SupplyContentRepository.Query
+                .Include(s => s.Product)
+                .Include(s => s.Supply),
+                x => x.Id == id);
+
+            _dataBaseManager.SupplyContentRepository.Remove(supplyContent);
+
+            await _dataBaseManager.SupplyContentRepository.CommitAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _dataBaseManager.Dispose();
+            }
+           
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WholesaleStore/Controllers/SupplyShipmentController.cs
@@ -0,0 +1,189 @@
+﻿using System.Data.Entity;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using WholesaleStore.Controllers.Base;
+using WholesaleStore.Data.Interfaces;
+
+namespace WholesaleStore.Controllers
+{
+    public class SupplyShipmentController : BaseController
+    {
+        public SupplyShipmentController(
+            IDataExecutor dataExecutor,
+            IGridManager gridManager,
+            IDataBaseManager dataBaseManager) : base(gridManager, dataExecutor, dataBaseManager)
+        {
+
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var supplyShipments = await _dataExecutor.ToListAsync(
+                _dataBaseManager.SupplyShipmentRepository.Query
+                .Include(s => s.Employee)
+                .Include(s => s.ProductsInStorage)
+                .Include(s => s.SupplyContent));
+
+            return View(supplyShipments);
+        }
+
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            
+            var supplyShipment = await _dataExecutor.FirstOrDefaultAsync(
+                _dataBaseManager.SupplyShipmentRepository.Query
+                .Include(s => s.Employee)
+                .Include(s => s.ProductsInStorage)
+                .Include(s => s.SupplyContent),
+                x => x.Id == id);
+
+            if (supplyShipment == null)
+            {
+                return HttpNotFound();
+            }
+            
+            return View(supplyShipment);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.EmployeeId = new SelectList(_dataBaseManager.EmployeeRepository.Query, "Id", "FirstName");
+            ViewBag.ProductInStorageId = new SelectList(_dataBaseManager.ProductsInStorageRepository.Query, "Id", "Id");
+            ViewBag.SupplyContentId = new SelectList(_dataBaseManager.SupplyContentRepository.Query, "Id", "Id");
+            
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(SupplyShipment supplyShipment)
+        {
+            if (ModelState.IsValid)
+            {
+                _dataBaseManager.SupplyShipmentRepository.Create(supplyShipment);
+
+                await _dataBaseManager.SupplyShipmentRepository.CommitAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.EmployeeId = new SelectList(_dataBaseManager.EmployeeRepository.Query, "Id", "FirstName", supplyShipment.EmployeeId);
+            ViewBag.ProductInStorageId = new SelectList(_dataBaseManager.ProductsInStorageRepository.Query, "Id", "Id", supplyShipment.ProductInStorageId);
+            ViewBag.SupplyContentId = new SelectList(_dataBaseManager.SupplyContentRepository.Query, "Id", "Id", supplyShipment.SupplyContentId);
+            
+            return View(supplyShipment);
+        }
+
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var supplyShipment = await _dataExecutor.FirstOrDefaultAsync(
+                _dataBaseManager.SupplyShipmentRepository.Query
+                .Include(s => s.Employee)
+                .Include(s => s.ProductsInStorage)
+                .Include(s => s.SupplyContent),
+                x => x.Id == id);
+
+            if (supplyShipment == null)
+            {
+                return HttpNotFound();
+            }
+            
+            ViewBag.EmployeeId = new SelectList(_dataBaseManager.EmployeeRepository.Query, "Id", "FirstName", supplyShipment.EmployeeId);
+            ViewBag.ProductInStorageId = new SelectList(_dataBaseManager.ProductsInStorageRepository.Query, "Id", "Id", supplyShipment.ProductInStorageId);
+            ViewBag.SupplyContentId = new SelectList(_dataBaseManager.SupplyContentRepository.Query, "Id", "Id", supplyShipment.SupplyContentId);
+            
+            return View(supplyShipment);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(SupplyShipment supplyShipment)
+        {
+            if (ModelState.IsValid)
+            {
+                var entity = await _dataExecutor.FirstOrDefaultAsync(
+                _dataBaseManager.SupplyShipmentRepository.Query
+                .Include(s => s.Employee)
+                .Include(s => s.ProductsInStorage)
+                .Include(s => s.SupplyContent),
+                x => x.Id == supplyShipment.Id);
+
+                entity.SupplyContentId = supplyShipment.SupplyContentId;
+                entity.ProductInStorageId = supplyShipment.ProductInStorageId;
+                entity.Date = supplyShipment.Date;
+                entity.Count = supplyShipment.Count;
+                entity.EmployeeId = supplyShipment.EmployeeId;
+
+                await _dataBaseManager.BrandRepository.CommitAsync();
+
+                return RedirectToAction("Index");
+            }
+        
+            ViewBag.EmployeeId = new SelectList(_dataBaseManager.EmployeeRepository.Query, "Id", "FirstName", supplyShipment.EmployeeId);
+            ViewBag.ProductInStorageId = new SelectList(_dataBaseManager.ProductsInStorageRepository.Query, "Id", "Id", supplyShipment.ProductInStorageId);
+            ViewBag.SupplyContentId = new SelectList(_dataBaseManager.SupplyContentRepository.Query, "Id", "Id", supplyShipment.SupplyContentId);
+         
+            return View(supplyShipment);
+        }
+
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var supplyShipment = await _dataExecutor.FirstOrDefaultAsync(
+                _dataBaseManager.SupplyShipmentRepository.Query
+                .Include(s => s.Employee)
+                .Include(s => s.ProductsInStorage)
+                .Include(s => s.SupplyContent),
+                x => x.Id == id);
+
+            if (supplyShipment == null)
+            {
+                return HttpNotFound();
+            }
+         
+            return View(supplyShipment);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            var supplyShipment = await _dataExecutor.FirstOrDefaultAsync(
+                _dataBaseManager.SupplyShipmentRepository.Query
+                .Include(s => s.Employee)
+                .Include(s => s.ProductsInStorage)
+                .Include(s => s.SupplyContent),
+                x => x.Id == id);
+
+            _dataBaseManager.SupplyShipmentRepository.Remove(supplyShipment);
+
+            await _dataBaseManager.SupplyShipmentRepository.CommitAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _dataBaseManager.Dispose();
+            }
+          
+            base.Dispose(disposing);
+        }
+    }
+}
